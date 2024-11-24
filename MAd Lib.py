3f4197583e6c938fa5ce060c5ae7a2069831a912
Waghmare Ravindra Shankar <100698590+Waghmareravindra@users.noreply.github.com>
2024-11-24 18:57:39 +0530

    Add files via upload

---
+++ MAd Lib.py
@@ -0,0 +1,7 @@
+color = input("Enter a color : ")
+pronoun = input("in mumbai which park: ")
+meaning_of_life = input("meaning of life: ")
+
+print("roses are "+ color)
+print("in mumbai we went "+ pronoun)
+print("what do you think about life: "+ meaning_of_life)
